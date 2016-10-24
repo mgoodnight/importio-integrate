ca8abbaacc0e3f513b8321dfa698b64eea9df4d3
Matt Goodnight <mbgoodnight@gmail.com>
2016-10-23 19:06:30 -0700

    Fix for indentation.

--- lib/ImportIO/Integrate.pm
+++ lib/ImportIO/Integrate.pm
@@ -61,7 +61,7 @@ sub live_extractor_query {
     unless ($self->target_url) { warn "No URL defined.  Live query will likely fail..." };
 
     my $query_url = $self->_generate_live_url;
-	my $content = $self->_query($query_url);
+    my $content = $self->_query($query_url);
 
     my $deserialized_content;
     try {
@@ -71,21 +71,21 @@ sub live_extractor_query {
         warn "There was an issue deserializing response content.";
     };
 
-	return $deserialized_content;
+    return $deserialized_content;
 }
 
 sub last_run_csv_query {
-	my $self = shift;
-	my $query_url = $self->_generate_csv_url;
-	my $content = $self->_query($query_url, $self->ua_headers);
+    my $self = shift;
+    my $query_url = $self->_generate_csv_url;
+    my $content = $self->_query($query_url, $self->ua_headers);
 
-	return $content;	
+    return $content;	
 }
 
 sub last_run_json_query {
-	my $self = shift;
-	my $query_url = $self->_generate_json_url;
-	my $content = $self->_query($query_url, $self->ua_headers);
+    my $self = shift;
+    my $query_url = $self->_generate_json_url;
+    my $content = $self->_query($query_url, $self->ua_headers);
 
     my $deserialized_content;
     try {
@@ -95,33 +95,33 @@ sub last_run_json_query {
         warn "There was an issue deserializing response content.";
     };
 
-	return $deserialized_content;
+    return $deserialized_content;
 }
 
 sub get_google_sheets_formula {
-	my $self = shift;
-	my $query_url = $self->_generate_csv_url;
-	my $formula = '=IMPORTDATA("' . $query_url . '")';
+    my $self = shift;
+    my $query_url = $self->_generate_csv_url;
+    my $formula = '=IMPORTDATA("' . $query_url . '")';
 
-	return $formula;
+    return $formula;
 }
 
 sub last_live_query_info {
-	my $self = shift;
-	my $query_url = $self->generate_rss_url;
-	my $content = $self->_query($query_url);
+    my $self = shift;
+    my $query_url = $self->generate_rss_url;
+    my $content = $self->_query($query_url);
 	
-	return $content;
+    return $content;
 }
 
 sub generate_rss_url {
-	my $self = shift;
-	my $url = 'https://rss.import.io/extractor/' . 
-			  $self->extractor_id .
+    my $self = shift;
+    my $url = 'https://rss.import.io/extractor/' . 
+              $self->extractor_id .
               '/runs?_apikey=' .
               $self->api_key;
 	
-	return $url;
+    return $url;
 }
 
 sub _generate_live_url {
@@ -133,41 +133,41 @@ sub _generate_live_url {
               '?_apikey=' .
               $self->api_key .
               '&url=' .
-               $uri_encoder->encode( $self->target_url );
-
-	return $url;
+              $uri_encoder->encode( $self->target_url );
+              
+    return $url;
 }
 
 sub _generate_csv_url {
-	my $self = shift;
-	my $url = 'https://data.import.io/extractor/' .
-			  $self->extractor_id .
-			  '/csv/latest?_apikey=' .
-			  $self->api_key;
+    my $self = shift;
+    my $url = 'https://data.import.io/extractor/' .
+              $self->extractor_id .
+              '/csv/latest?_apikey=' .
+              $self->api_key;
 
-	return $url;
+    return $url;
 }
 
 sub _generate_json_url {
-	my $self = shift;
-	my $url = 'https://data.import.io/extractor/' .
+    my $self = shift;
+    my $url = 'https://data.import.io/extractor/' .
               $self->extractor_id .
               '/json/latest?_apikey=' .
               $self->api_key;
 
-	return $url;
+    return $url;
 }
 
 sub _query {
-	my ($self, $extractor_url, $headers) = @_;
+    my ($self, $extractor_url, $headers) = @_;
 
-	my $response = $self->ua->get($extractor_url, $headers);
-	$self->_set_last_query_response($response);
+    my $response = $self->ua->get($extractor_url, $headers);
+    $self->_set_last_query_response($response);
 	
-	my $content_decoded = $response->decoded_content;
-	$self->_set_last_query_error($content_decoded) if !$response->is_success;
+    my $content_decoded = $response->decoded_content;
+    $self->_set_last_query_error($content_decoded) if !$response->is_success;
 	
-	return $content_decoded;
+    return $content_decoded;
 }
 
 1;
